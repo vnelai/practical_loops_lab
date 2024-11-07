7c8de82dae525f39c43b8b07db14399545bcfd58
vnelai <118484488+vnelai@users.noreply.github.com>
2024-11-07 14:03:23 -0500

    Loop through all numbers from 1 to 100

---
+++ index.js
@@ -0,0 +1,18 @@
+// Practical Loops ALAB 308.3.1
+
+// Part 1: Fizz Buzz
+
+// Loop through all numbers from 1 to 100.
+// Case 1: (Best way in my opinion due to the efficiency )
+for (let i = 1; i <= 100; i++){
+    console.log(i);
+}
+// Case 2:
+let i = 1;
+while (i <= 100){
+    console.log(i);
+    i++
+}
+
+
+
