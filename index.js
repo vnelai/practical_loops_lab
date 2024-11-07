df8e04c5475d7f2a558db0257ea5782c16f71451
vnelai <118484488+vnelai@users.noreply.github.com>
2024-11-07 14:18:24 -0500

    If a number is divisible by both 3 and 5, log “Fizz Buzz.”

--- index.js
+++ index.js
@@ -46,3 +46,22 @@ while (i <= 100) {
     }
     i++;
 }
+
+
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// Case 1:
+for (i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("Fizz Buzz");
+    }
+}
+
+// Case 2:
+let i = 1;
+while (i <= 100) {
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("Fizz Buzz")
+    }
+    i++ ;
+}
+
